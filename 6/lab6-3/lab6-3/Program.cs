bd4f948960648ea89b0462d2f0faebc299133295
JAMshyt <77636014+JAMshyt@users.noreply.github.com>
2023-10-24 12:46:10 +0300

    Add files via upload

---
+++ 6/lab6-3/lab6-3/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Xml.Linq;
+
+
+namespace lab6_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Таблица";
+            XDocument Документ = new XDocument(
+                new XElement("Таблица",
+                    new XElement("Строка",
+                        new XAttribute("N_ПП", "72"),
+                        new XElement("Фамилия", "Уласевич"),
+                        new XElement("Имя", "Мария"),
+                        new XElement("Отчество", "Ивановна"),
+                        new XElement("Дата_рождения", "05.01.1955"),
+                        new XElement("Образов", "среднее спец"),
+                        new XElement("Дата_зачисления", "01.08.1973"),
+                        new XElement("Должность", "бухгалтер"),
+                        new XElement("Специальность", "бухгалтер")
+                        ),
+                    new XElement("Строка",
+                        new XAttribute("N_ПП", "73"),
+                        new XElement("Фамилия", "Юркевич"),
+                        new XElement("Имя", "Любовь"),
+                        new XElement("Отчество", "Васильевна"),
+                        new XElement("Дата_рождения", "14.07.1957"),
+                        new XElement("Образов", "среднее спец"),
+                        new XElement("Дата_зачисления", "25.06.1984"),
+                        new XElement("Должность", "контролер"),
+                        new XElement("Специальность", "гидротехник")
+                        ),
+                    new XElement("Строка",
+                        new XAttribute("N_ПП", "11"),
+                        new XElement("Фамилия", "Будыкина"),
+                        new XElement("Имя", "Галина"),
+                        new XElement("Отчество", "Константиновна"),
+                        new XElement("Дата_рождения", "12.05.1961"),
+                        new XElement("Образов", "среднее спец"),
+                        new XElement("Дата_зачисления", "28.04.1990"),
+                        new XElement("Должность", "контролер"),
+                        new XElement("Специальность", "техник-электрик")
+                        ),
+                    new XElement("Строка",
+                        new XAttribute("N_ПП", "13"),
+                        new XElement("Фамилия", "Вакарев"),
+                        new XElement("Имя", "Вячеслав"),
+                        new XElement("Отчество", "Васильевич"),
+                        new XElement("Дата_рождения", "23.03.1941"),
+                        new XElement("Образов", "среднее спец"),
+                        new XElement("Дата_зачисления", "16.02.1992"),
+                        new XElement("Должность", "старший контролер"),
+                        new XElement("Специальность", "техник радиосвязи")
+                        ),
+                    new XElement("Строка",
+                        new XAttribute("N_ПП", "64"),
+                        new XElement("Фамилия", "Трусов"),
+                        new XElement("Имя", "Игорь"),
+                        new XElement("Отчество", "Евгеньевич"),
+                        new XElement("Дата_рождения", "12.08.1948"),
+                        new XElement("Образов", "среднее спец"),
+                        new XElement("Дата_зачисления", "28.06.1996"),
+                        new XElement("Должность", "контролер"),
+                        new XElement("Специальность", "техник электрик")
+                        ),
+                    new XElement("Строка",
+                        new XAttribute("N_ПП", "18"),
+                        new XElement("Фамилия", "Гринько "),
+                        new XElement("Имя", "Наталья"),
+                        new XElement("Отчество", "Никитична"),
+                        new XElement("Дата_рождения", "01.01.1960"),
+                        new XElement("Образов", "среднее спец"),
+                        new XElement("Дата_зачисления", "24.04.1998"),
+                        new XElement("Должность", "контролер"),
+                        new XElement("Специальность", "техник-плановик")
+                        ),
+                    new XElement("Строка",
+                        new XAttribute("N_ПП", "5"),
+                        new XElement("Фамилия", "Бибко "),
+                        new XElement("Имя", "Владимир"),
+                        new XElement("Отчество", "Михайлович"),
+                        new XElement("Дата_рождения", "01.01.1960"),
+                        new XElement("Образов", "среднее спец"),
+                        new XElement("Дата_зачисления", "24.04.1998"),
+                        new XElement("Должность", "техгик"),
+                        new XElement("Специальность", "техник-строитель")
+                        ),
+                    new XElement("Строка",
+                        new XAttribute("N_ПП", "10"),
+                        new XElement("Фамилия", "Бородко "),
+                        new XElement("Имя", "Татьяна"),
+                        new XElement("Отчество", "Николаевна"),
+                        new XElement("Дата_рождения", "01.01.1960"),
+                        new XElement("Образов", "среднее спец"),
+                        new XElement("Дата_зачисления", "24.04.1998"),
+                        new XElement("Должность", "контролер"),
+                        new XElement("Специальность", "инжинер-экономитс")
+                        )
+                )
+                );
+            Документ.Save(@"..\..\lab6-3.XML");
+            Console.WriteLine(Документ);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 6/lab6_2/lab6_2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Xml.Linq;
+
+
+namespace lab6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "XML";
+            XDocument Документ = new XDocument(
+                new XElement("Доходовые_сведенья",
+                    new XElement("Строка",
+                        new XAttribute("Источник", "Продажи камней"),
+                        new XElement("Январь", 100),
+                        new XElement("Февраль", 200),
+                        new XElement("Март", 150),
+                        new XElement("Апрель", 125),
+                        new XElement("Май", 110),
+                        new XElement("Июнь", 90),
+                        new XElement("Июль", 80),
+                        new XElement("Август", 80),
+                        new XElement("Сентябрь", 40),
+                        new XElement("Октябрь", 40),
+                        new XElement("Ноябрь", 80),
+                        new XElement("Декабрь", 100)
+                        ),
+                    new XElement("Строка",
+                        new XAttribute("Источник", "Продажи пожарных гидрантов"),
+                        new XElement("Январь", 20000),
+                        new XElement("Февраль", 30000),
+                        new XElement("Март", 34000),
+                        new XElement("Апрель", 40000),
+                        new XElement("Май", 20000),
+                        new XElement("Июнь", 25000),
+                        new XElement("Июль", 27000),
+                        new XElement("Август", 32000),
+                        new XElement("Сентябрь", 40000),
+                        new XElement("Октябрь", 37000),
+                        new XElement("Ноябрь", 35000),
+                        new XElement("Декабрь", 32000)
+                        ),
+                    new XElement("Строка",
+                        new XAttribute("Источник", "Поставки оружия"),
+                        new XElement("Январь", 100000),
+                        new XElement("Февраль", 100000),
+                        new XElement("Март", 125000),
+                        new XElement("Апрель", 150000),
+                        new XElement("Май", 175000),
+                        new XElement("Июнь", 190000),
+                        new XElement("Июль", 230000),
+                        new XElement("Август", 270000),
+                        new XElement("Сентябрь", 300000),
+                        new XElement("Октябрь", 222000),
+                        new XElement("Ноябрь", 170000),
+                        new XElement("Декабрь", 90000)
+                        ),
+                    new XElement("Строка",
+                        new XAttribute("Источник", "Толкания пыли"),
+                        new XElement("Январь", 1000000),
+                        new XElement("Февраль", 900000),
+                        new XElement("Март", 980000),
+                        new XElement("Апрель", 960000),
+                        new XElement("Май", 920000),
+                        new XElement("Июнь", 1200000),
+                        new XElement("Июль", 1150000),
+                        new XElement("Август", 1050000),
+                        new XElement("Сентябрь", 900000),
+                        new XElement("Октябрь", 930000),
+                        new XElement("Ноябрь", 920000),
+                        new XElement("Декабрь", 950000)
+                        ),
+                    new XElement("Строка",
+                        new XAttribute("Источник", "Доходы с рекламы"),
+                        new XElement("Январь", 18),
+                        new XElement("Февраль", 15),
+                        new XElement("Март", 20),
+                        new XElement("Апрель", 40),
+                        new XElement("Май", 44),
+                        new XElement("Июнь", 42),
+                        new XElement("Июль", 48),
+                        new XElement("Август", 44),
+                        new XElement("Сентябрь", 46),
+                        new XElement("Октябрь", 42),
+                        new XElement("Ноябрь", 48),
+                        new XElement("Декабрь", 42)
+                        )
+                )
+                );
+            Документ.Save(@"..\..\lab6-2.XML");
+            Console.WriteLine(Документ);
+            Console.ReadKey();
+        }
+    }
+}
